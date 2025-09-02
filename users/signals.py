fc81371f29268c94205a3c8d0982191a81a88aab
Md Masud <masudhasan1680@gmail.com>
2025-09-02 13:02:46 +0600

    group list added

--- users/signals.py
+++ users/signals.py
@@ -1,7 +1,7 @@
 #signals import
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django.conf import settings
@@ -26,7 +26,12 @@ def send_activation_email(sender, instance, created, **kwargs):
             print(f'Faield to send email to {instance.email} : {str(e)}')
         
         
-        
+@receiver(post_save, sender=User)
+def assign_role(sender, instance, created, **kwargs):
+    if created:
+        user_group, created = Group.objects.get_or_create(name='user')
+        instance.group.add(user_group)
+        instance.save()       
         
         
         
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User 
-from users.forms import  CustomRegisterForm, CustomLoginForm
+from django.contrib.auth.models import User , Group
+from users.forms import  CustomRegisterForm, CustomLoginForm, AssignRoleForm, CreateGroupForm
 from django.contrib import messages
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.tokens import default_token_generator
@@ -91,3 +91,137 @@ def active_user(request, user_id, token):
     
     except User.DoesNotExist:
         return HttpResponse('User not found!!')
+    
+    
+def admin_dashboard(request):
+    users = User.objects.all()
+    return render(request, 'admin/dashboard.html', {"users" : users})
+
+def assign_role(request, user_id):
+    user = User.objects.get(id = user_id)
+    
+    form = AssignRoleForm()
+    
+    if request.method == 'POST':
+        form = AssignRoleForm(request.POST)
+        if form.is_valid():
+            role = form.cleaned_data.get('role')
+            user.groups.clear() # remove old roles
+            user.groups.add(role)
+            
+            messages.success(request, f"User {user.username} has been assigned to the {role.name} role.")
+            return redirect('admin-dashboard')
+    
+    return render(request, 'admin/assigned_role.html', {"form" : form})
+
+
+def create_group(request):
+    form = CreateGroupForm()
+    
+    if request.method == 'POST':
+        form = CreateGroupForm(request.POST)
+        if form.is_valid():
+            group = form.save()
+            messages.success(request, f"Group {group.name} has been created successfully.")
+            return redirect('create-group')
+    return render(request, 'admin/create_group.html', {"form" : form})
+
+def group_list(request):
+    groups = Group.objects.all()
+    
+    return render(request, 'admin/group_list.html', {"groups" : groups})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
